a49ca474b0c53d2ae8caea92a099a86b418df022
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-15 19:30:22 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,20 +3,13 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-#First we need to define a class EmailParser.
-
 class EmailParser
-#Then our instances need to be initialized with an email string.
-#I want the instance of EmailParser to have a property "list" where the string of emails will be stored.
   attr_accessor :list
   
-#first, upon instantiation, store the string to an instance variable.
   def initialize(email_list)
     @list = email_list
   end
 
-#Then, go through that list and split them into unique emails.
-#How can I parse either by CSV OR Space emails?
   def parse
     self.list.split(/,\s|\s/).uniq
   end
